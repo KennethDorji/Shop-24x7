3d9339313e89eef2237086f6cc6a5fccd63c4cd4
Kenneth Dorji <kennethdorji@gmail.com>
2021-07-25 15:07:54 -0400

    Change message

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", async (req, res) => res.send("Server is running."));
+app.get("/", async (req, res) => res.send("Server is running"));
 
 app.listen(3000, () => console.log("Server running on port 3000"));
