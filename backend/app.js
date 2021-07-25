0551d068078722a85633698814534df67b8efc19
Kenneth Dorji <kennethdorji@gmail.com>
2021-07-25 14:11:10 -0400

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", async (req, res) => res.send("Server is running."));
+
+app.listen(3000, () => console.log("Server running on port 3000"));
